fc1dabe545a84ba337deeec6945820eabec28aa2
Cecchetti <pedro.cecchetti@gmail.com>
2020-03-01 00:18:52 +0100

    feat: Added BlogPosts App

---
+++ posts/models.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.db import models
+
+
+class BlogPost(models.Model):
+    """
+    This Class implements all the information related to all the blog posts that will
+    be hosted on a post
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    title = models.CharField(max_length=128, verbose_name='Título')
+    text = models.TextField(verbose_name='Texto Completo')
+
+    author = models.ForeignKey(to='users.User', on_delete=models.SET_NULL, null=True)
+
+    # Monitoring
+    last_login = models.DateTimeField(null=True, verbose_name="Ultimo login")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Ultima atualização")
+
+    class Meta:
+        verbose_name = 'Blog Post'
+        verbose_name_plural = 'Blog Posts'
+
+    def __str__(self):
+        return f'{self.title - self.author.get_full_name()}'
