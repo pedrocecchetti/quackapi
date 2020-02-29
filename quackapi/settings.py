a13ff6152bca34b7a96f508d94972997ab231f25
Cecchetti <pedro.cecchetti@gmail.com>
2020-02-29 13:13:49 +0100

    feat: Created DB config to PostgreSQL

--- quackapi/settings.py
+++ quackapi/settings.py
@@ -72,13 +72,17 @@ WSGI_APPLICATION = 'quackapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "postgres",
+        'HOST': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
